b19c20075e13621672263ea0b144ee959a8672af
tuanzi <tuanzi@2dfire.com>
2017-01-03 18:45:28 +0800

    update download util

--- src/main/java/com/shaw/utils/DownloadUtils.java
+++ src/main/java/com/shaw/utils/DownloadUtils.java
@@ -1,69 +1,44 @@
 package com.shaw.utils;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpClientParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
  * Created by Administrator on 2017/1/3 0003.
  */
-@Component
 public class DownloadUtils {
     //下载相关常量配置
+    //连接超时
     public static final long TIMEOUT = 5000;
+    //下载超时 1min
+    public static final long READTIMEOUT = 60 * 1000;
+    // 连接UA
     public static final String UA = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.1599.101 Safari/537.36";
+    // referer  pixiv 插画下载必须设置，否则403
     public static final String REFER = "http://spapi.pixiv.net/";
-    public static final Header UAHeader = new Header("User-Agent", UA);
-    public static final Header REFHeader = new Header("Referer", REFER);
 
-    public static HttpClient httpClient;
     public static Logger downloadLogger = LoggerFactory.getLogger(DownloadUtils.class);
 
-    static {
-        HttpClientParams clientParams = new HttpClientParams();
-        clientParams.setConnectionManagerTimeout(TIMEOUT);
-        httpClient = new HttpClient(clientParams);
-    }
-
     public static String downloadByUrl(String url, String savePath) {
-        GetMethod method = new GetMethod(url);
-        method.setRequestHeader(REFHeader);
-        method.setRequestHeader(UAHeader);
         try {
-            httpClient.executeMethod(method);
-            InputStream is = method.getResponseBodyAsStream();
+            URL connUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) connUrl.openConnection();
+            setHeader(conn);
+            InputStream is = conn.getInputStream();
             File file = new File(savePath);
             if (!file.exists()) file.mkdirs();
             savePath = file.getAbsolutePath() + "/" + url.substring(url.lastIndexOf("/"));
-            if (new File(savePath).exists()) {
-                downloadLogger.info("Existing file with the same name, deemed to have been downloaded");
-                return savePath;
-            } else {
-                //使用文件内存映射保存文件
-                RandomAccessFile randomAccessFile = new RandomAccessFile(savePath, "rw");
-                FileChannel channel = randomAccessFile.getChannel();
-                MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, method.getResponseContentLength());
-                byte[] bytes = new byte[2 * 1024];
-                int len;
-                while ((len = is.read(bytes)) != -1) {
-                    buffer.put(bytes, 0, len);
-                }
-                buffer.force();
-                channel.close();
-                randomAccessFile.close();
-                downloadLogger.info(String.format("%s Download Completed", savePath));
-                return savePath;
-            }
+            return saveStreamToFile(is, savePath, conn.getContentLengthLong());
         } catch (Exception e) {
             downloadLogger.error(String.format("%s Download Error", url));
             downloadLogger.error("Exception：", e);
@@ -71,4 +46,35 @@ public class DownloadUtils {
         }
     }
 
+    //设置请求头
+    private static void setHeader(URLConnection conn) {
+        conn.setRequestProperty("User-Agent", UA);
+        conn.setRequestProperty("Referer", REFER);
+        conn.setRequestProperty("Accept-Language", "en-us,en;q=0.7,zh-cn;q=0.3");
+        conn.setRequestProperty("Accept-Encoding", "utf-8");
+        conn.setRequestProperty("Keep-Alive", "300");
+        conn.setRequestProperty("connnection", "keep-alive");
+        conn.setConnectTimeout((int) TIMEOUT);
+        conn.setReadTimeout((int) READTIMEOUT);
+    }
+
+    //使用文件内存映射保存文件
+    private static String saveStreamToFile(InputStream is, String savePath, long contentLength) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(savePath, "rw");
+        FileChannel channel = randomAccessFile.getChannel();
+        try {
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, contentLength);
+            byte[] bytes = new byte[2 * 1024];
+            int len;
+            while ((len = is.read(bytes)) != -1) {
+                buffer.put(bytes, 0, len);
+            }
+            buffer.force();
+            downloadLogger.info(String.format("%s Download Completed", savePath));
+            return savePath;
+        } finally {
+            channel.close();
+            randomAccessFile.close();
+        }
+    }
 }
