bdf49ec9d27ea5e5ed32e97e77697438f3c387cb
Shaw <imn5100@users.noreply.github.com>
2017-01-11 18:01:25 +0800

    update Auth and checkChannelMap

--- src/main/java/com/shaw/netty/SimpleMessageServerHandler.java
+++ src/main/java/com/shaw/netty/SimpleMessageServerHandler.java
@@ -1,6 +1,7 @@
 package com.shaw.netty;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
@@ -12,7 +13,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.util.Map;
 import java.util.concurrent.*;
 
 
@@ -24,10 +24,11 @@ import java.util.concurrent.*;
 public class SimpleMessageServerHandler extends SimpleChannelInboundHandler<String> {
 
     public static Logger logger = LoggerFactory.getLogger(SimpleMessageServerHandler.class);
+    public static final String USER_AUTH_MAP = "UserAuthMap";
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
-
+    //用于保存连接的channel，通过appkey 获取对应连接，给指定客户端发送数据 如果channel失效，从map中移除。
     public static ConcurrentMap<String, Channel> channelMap = new ConcurrentHashMap<String, Channel>();
 
     public static void checkChannelMap() {
@@ -40,7 +41,6 @@ public class SimpleMessageServerHandler extends SimpleChannelInboundHandler<Stri
                     logger.info(key + " is Remove from channelMap");
                 }
             } else {
-                //hashMap 运行null的键值对，所以null需要移除
                 SimpleMessageServerHandler.channelMap.remove(key);
                 logger.info(key + " is Remove from channelMap");
             }
@@ -63,12 +63,15 @@ public class SimpleMessageServerHandler extends SimpleChannelInboundHandler<Stri
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
+        ctx.close();
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        //@Deprecated 如果有大量无效请求，该时间会频繁触发，所以不再这里检查所有的连接的有效性。改为定时器触发
         //每当有channel失效时，重新检查Map
-        checkChannelMap();
+//        checkChannelMap();
+
     }
 
     @Override
@@ -84,14 +87,28 @@ public class SimpleMessageServerHandler extends SimpleChannelInboundHandler<Stri
         }
         if (message.getType() == 1) {
             if (!StringUtils.isEmpty(message.getAppKey())) {
-                //当重复连接时，移除上一个连接
-                Channel channel = channelMap.get(message.getAppKey());
-                if (channel != null && channel.isActive()) {
-                    logger.info("remove old channel. appkey:" + message.getAppKey());
-                    channel.close();
+                String info = (String) redisTemplate.opsForHash().get(USER_AUTH_MAP, message.getAppKey());
+                //未通过登录验证
+                if (info == null || message.getAppSecret() == null) {
+                    ctx.close();
+                    return;
+                } else {
+                    JSONObject infoObject = JSONObject.parseObject(info);
+                    String appSercet = infoObject.getString("appSercet");
+                    if (message.getAppSecret().equals(appSercet)) {
+                        //登录验证通过
+                        //当重复连接时，移除上一个连接
+                        Channel channel = channelMap.get(message.getAppKey());
+                        if (channel != null && channel.isActive()) {
+                            logger.info("remove old channel. appkey:" + message.getAppKey());
+                            channel.close();
+                        }
+                        logger.info("put channel. appkey:" + message.getAppKey());
+                        channelMap.put(message.getAppKey(), ctx.channel());
+                    } else {
+                        ctx.close();
+                    }
                 }
-                logger.info("put channel. appkey:" + message.getAppKey());
-                channelMap.put(message.getAppKey(), ctx.channel());
             } else {
                 ctx.close();
             }
